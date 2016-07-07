367b854597e4599cac251695f6b23b3ce2366dd5
eaharonoff <emanuel.aharonoff@gmail.com>
2016-07-07 15:59:49 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true 
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :title, format: {with: /Won't Believe| Secret | Top
+| Guess / } 
 end
+
+
